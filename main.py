4930b8b2ac80eacba279954cbced1f0e2c05feeb
zoltanas <108790802+zoltanas@users.noreply.github.com>
2024-01-07 01:27:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ import re
 import random
 
 service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-extensions')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=service, options=options)
 driver.set_window_size(1920, 1080)
 
 #Random times between actions, so it looks like human)
