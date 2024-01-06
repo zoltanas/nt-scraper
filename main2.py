1851dcebdea8a4c346b39c29bc07f9f5ba16024e
zoltanas <108790802+zoltanas@users.noreply.github.com>
2024-01-07 00:54:11 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -21,7 +21,7 @@ def setup():
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
     driver = webdriver.Chrome(service=service, options=options)
